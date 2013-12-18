247612005e6a30814b40a4bb6934de3bdd4512ec
wkjagt <wkjagt@gmail.com>
2013-12-17 20:57:16 -0500

    replace jquery version of js library with vaniall

--- testsite/infrared.js
+++ testsite/infrared.js
@@ -1,34 +1,25 @@
-(function( $, undefined ){
-    "use strict";
-    var pluginGlobals = {}
+"use strict";
 
-    var storage = {
-        fallback : [],
+var Infrared = {
+    pluginGlobals : {},
+
+    storage : {
         init : function(flush) {
             flush == flush || false;
 
             if(flush) {
-                this.fallback = [];
-                if(typeof localStorage.infrared != 'undefined') {
-                    localStorage.infrared = JSON.stringify([]);
-                }
+                localStorage.infrared = JSON.stringify([]);
             }
         },
         store : function(data) {
+            var stored = JSON.parse(this.getStored());
+            stored.push(data);
+            localStorage.infrared = JSON.stringify(stored);
 
-            if(pluginGlobals.use_storage) {
-                var stored = this.getStored();
-                stored.push(data);
-                localStorage.infrared = JSON.stringify(stored);
-
-                if(stored.length >= pluginGlobals.max_storage) {
-                    return true;
-                }
-                return false;
-            } else {
-                this.fallback.push(data)
+            if(stored.length >= Infrared.pluginGlobals.max_storage) {
                 return true;
             }
+            return false;
         },
         flush : function() {
             var stored = this.getStored();
@@ -36,104 +27,63 @@
             return stored;
         },
         getStored : function() {
-            if(this.fallback.length) {
-                var stored = this.fallback;
-                this.init(true);
-                return stored;
-            }
-            if(typeof localStorage.infrared === 'undefined') {
-                return [];
-            }
-            return JSON.parse(localStorage.infrared);
+            return localStorage.infrared;
         }
-    };
-
-    var methods = {
-        validateOptions : function(options) {
-            var required = ['server_endpoint'];
-
-            for(var i in required) {
-                if(typeof options[required[i]] == 'undefined') {
-                    $.error('infrared error : "'+required[i]+'" missing in options');
-                }                
-            }
-
-        },
-        init : function( options ) {
-
-            methods.validateOptions(options);
-
-            pluginGlobals = {
-                use_storage : typeof localStorage == 'object' && typeof JSON == 'object',
-                max_storage : options.max_storage || 1,
-                server_endpoint : options.server_endpoint,
-                startTime : new Date().getTime(),
-                totalClicks : 0,
-                timeCutoff : options.time_cutoff || false, //ms or false
-                clickCutoff : options.click_cutoff || false,
-                centered : options.centered || false,
-            };
-            storage.init();
-
-            $('body').addClass('infrared_enabled');
-
-            return $(document).on('mousedown.infrared', methods.report);
-        },
-        report : function( event ) {
-
-            pluginGlobals.totalClicks++;
-
-            var now = new Date().getTime()
-                ,$this = $(this)
-                ,full = false
-                ,elapsed = now - pluginGlobals.startTime;
-
-            if((pluginGlobals.clickCutoff !== false && pluginGlobals.totalClicks > pluginGlobals.clickCutoff)
-                || (pluginGlobals.timeCutoff !== false && elapsed > pluginGlobals.timeCutoff)) {
-                return;
-            }
-
-            var entry = {
-                click: {
-                    x : pluginGlobals.centered ? (event.pageX - (document.body.clientWidth/2)) : event.pageX,
-                    y: event.pageY,
-                },
-                elapsed : elapsed,
-                centered : pluginGlobals.centered,
-                page : window.location.pathname
-            };
-
-            full = storage.store(entry);
-
-            if(!full) {
-                return;
-            }
-
-            $.ajax({
-                type: 'POST',
-                url: pluginGlobals.server_endpoint+'/api/domains/'+window.location.hostname+'/clicks',
-                data: {
-                    clicks: storage.flush()
-                },
-                dataType: 'json',
-                success : function(){}
-            });//.abort(); // abort immediately. we only want to send. response is irrelevant
-        },
-        reset : function( ) {
-            pluginGlobals.startTime = new Date().getTime();
-            pluginGlobals.totalClicks = 0;
-        },
-        setpage: function( page ) {
-            pluginGlobals.page = page;
+    },
+    init : function( options ) {
+        if (!window.XMLHttpRequest || !JSON || !localStorage) {
+            // we don't support this browser
+            return;
         }
-    };
+        this.pluginGlobals = {
+            max_storage : 1,
+            server_endpoint : 'http://dev.infraredapp.com',
+            startTime : new Date().getTime(),
+            totalClicks : 0,
+            centered : options.centered || false,
+        };
+        this.storage.init();
 
-    $.fn.infrared = function( arg ) {
-        // Method calling logic
-        if ( methods[arg] ) {
-            return methods[ arg ].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else {
-            $.error('Method ' + method + ' does not exist on jQuery.infrared');
-        }        
-    };
-})( jQuery );
+        document.body.className += " infrared_enabled";
+        document.body.onmousedown = this.report;
+    },
+    report : function( event ) {
+
+        Infrared.pluginGlobals.totalClicks++;
+
+        var now = new Date().getTime()
+            ,full = false
+            ,elapsed = now - Infrared.pluginGlobals.startTime;
+
+        var entry = {
+            click: {
+                x : Infrared.pluginGlobals.centered ? (event.pageX - (document.body.clientWidth/2)) : event.pageX,
+                y: event.pageY,
+            },
+            elapsed : elapsed,
+            centered : Infrared.pluginGlobals.centered,
+            page : window.location.pathname
+        };
+
+        full = Infrared.storage.store(entry);
+
+        if(!full) {
+            return;
+        }
+
+        var url = Infrared.pluginGlobals.server_endpoint+'/api/domains/'+window.location.hostname+'/clicks',
+            x = new XMLHttpRequest(),
+            data = Infrared.storage.flush();
+
+        x.open("POST", url, true);
+        x.setRequestHeader('Content-type', 'application/json');
+        x.send(data);
+    },
+    reset : function( ) {
+        pluginGlobals.startTime = new Date().getTime();
+        pluginGlobals.totalClicks = 0;
+    },
+    setpage: function( page ) {
+        pluginGlobals.page = page;
+    }
+}
