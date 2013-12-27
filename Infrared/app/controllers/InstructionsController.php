74484f2d81c851da7a5534321228ac5bf8e76eac
wkjagt <wkjagt@gmail.com>
2013-12-27 10:29:25 -0500

    fix domain in instructions #20

--- Infrared/app/controllers/InstructionsController.php
+++ Infrared/app/controllers/InstructionsController.php
@@ -8,6 +8,8 @@ class InstructionsController extends \Phalcon\Mvc\Controller
             return $this->response->redirect(array('for' => 'front'));
         }
         $this->view->setVar("active_menu", 'instructions');
+        $this->view->setVar("host", $this->request->getHttpHost());
+        $this->view->setVar("scheme", $this->request->getScheme());
     }
 
     public function indexAction() {}
