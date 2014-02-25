834e9743b5806344a8ac292305d90e112ec5cfe6
wkjagt <wkjagt@gmail.com>
2014-02-25 10:04:55 -0500

    minimalist controller based acl

--- Infrared/app/plugins/Security.php
+++ Infrared/app/plugins/Security.php
@@ -2,16 +2,27 @@
 
 use Phalcon\Events\Event,
         Phalcon\Mvc\User\Plugin,
-        Phalcon\Mvc\Dispatcher,
-        Phalcon\Acl;
+        Phalcon\Mvc\Dispatcher;
 
 class Security extends Plugin
 {
+    /**
+     * If this gets more granular, use proper Acl classes
+     */
     public function beforeDispatch(Event $event, Dispatcher $dispatcher)
     {
         $auth = $this->session->get('auth');
         if($auth && $user = User::findFirst($auth['user_id'])) {
             $di = $this->getDI()->set('user', $user);
+        }
+
+        $privateControllers = array('domains', 'instructions');
+
+        $controller = $dispatcher->getControllerName();
+
+        if(!$auth && in_array($controller, $privateControllers)) {
+            $this->response->redirect(array('for' => 'front'));
+            return false;
         }
     }
 }
